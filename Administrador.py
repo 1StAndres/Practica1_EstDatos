dfd95e51256b0107a1353093a75c56e0e3d5cd0d
damazog <damazog@unal.edu.co>
2025-01-13 18:01:46 -0500

    cositas

--- Administrador.py
+++ Administrador.py
@@ -1,5 +1,23 @@
 from Investigador import Investigador
 
 class Administrador(Investigador):
-    def __init__(self):
-        super().__init__()
+
+    solicitudes_nuevo = []
+    solicitudes_eliminar = [] 
+
+    def __init__(self, nombre, id, fecha_nacimiento, ciudad_nacimiento, tel, email, dir, lista_equipos):
+        super().__init__(nombre, id, fecha_nacimiento, ciudad_nacimiento, tel, email, dir)
+        self._lista_equipos = lista_equipos
+    
+    def revisar_solicitudes_nuevo(self):
+        for solicitud in Administrador.solicitudes_nuevo:
+            print(solicitud[0])
+            resultado =input("A continuación escriba Aprobado o Desaprobado")
+            if resultado == "Aprobado":
+                
+            if resultado == "Desaprobado":
+
+            else:
+                print("Por favor copie Aprobado o Desaprobado")
+
+    #def revisar_solicitudes_eliminar():
--- Investigador.py
+++ Investigador.py
@@ -1,4 +1,5 @@
 from Usuario import Usuario
+from Administrador import Administrador
 
 
 class Investigador(Usuario):
@@ -7,3 +8,9 @@ class Investigador(Usuario):
         self._lista_equipos = "None"
 
     def solicitar_nuevo(self, equipo):
+        Administrador.solicitudes_nuevo.append(equipo)
+    #aca se pide todo el objeto de tipo equipo
+    def solicitar_eliminar(self, numero_placa, justificacion):
+        solicitud = ["Equipo con número de placa:" + str(numero_placa) + ", Razón: " + justificacion, str(self.getId())]
+        Administrador.solicitudes_eliminar.append(solicitud)
+    #aca se pide una justificacion, y un solo atributo del objeto equipo
