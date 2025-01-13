ddd8a8d92bf9a4c765dfaa9ac736a89cffed9569
1StAndres <160525045+1StAndres@users.noreply.github.com>
2025-01-13 17:15:07 -0500

    Funcion menu y clase hora

--- Hora.py
+++ Hora.py
@@ -1,1 +1,20 @@
+class Hora:
+  def __init__(self,hora,minutos):#No se si incluir segundos
+    self.hora = hora
+    self.minutos = minutos
+  
+  def getHora(self):
+    return self.Hora
+  
+  def getMinutos(self):
+    return self.minutos
+  
+  def setHora(self, hora):
+    self.hora = hora
+  
+  def setMinutos(self, minutos):
+    self.minutos = minutos
 
+  def setAll(self,hora,minutos):
+    self.hora = hora
+    self.minutos = minutos
