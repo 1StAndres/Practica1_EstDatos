b2d43a493308df69f150a10dd79e292a64aa6004
damazog <damazog@unal.edu.co>
2025-01-15 16:49:01 -0500

    Update Investigador.py

--- Investigador.py
+++ Investigador.py
@@ -1,7 +1,6 @@
 from DoubleList import DoubleList
 from Usuario import Usuario
 
-
 class Investigador(Usuario):
     def __init__(self, nombre, id, fecha_nacimiento, ciudad_nacimiento, tel, email, dir): 
         super().__init__(nombre, id, fecha_nacimiento, ciudad_nacimiento, tel, email, dir)
